1078396d01ff65afd2f5bcd25d3c8f8c060f8751
emadkhan713 <emadkhan713@gmail.com>
2023-04-18 20:45:03 +1000

    fixing up the README and tidied up some of the functions & constants

--- Firetruck_PSXNew/Firetruck_PSXNew.ino
+++ Firetruck_PSXNew/Firetruck_PSXNew.ino
@@ -1,12 +1,11 @@
 // jump to line 122 for relevant definitions
 #include <DigitalIO.h>
 #include <PsxControllerBitBang.h>
-#include <L298NX2.h>
 #include <avr/pgmspace.h>
 
-typedef const __FlashStringHelper * FlashStr;
+typedef const __FlashStringHelper* FlashStr;
 typedef const byte* PGM_BYTES_P;
-#define PSTR_TO_F(s) reinterpret_cast<const __FlashStringHelper *> (s)
+#define PSTR_TO_F(s) reinterpret_cast<const __FlashStringHelper*>(s)
 
 const char buttonSelectName[] PROGMEM = "Select";
 const char buttonL3Name[] PROGMEM = "L3";
@@ -26,82 +25,82 @@ const char buttonCrossName[] PROGMEM = "Cross";
 const char buttonSquareName[] PROGMEM = "Square";
 
 const char* const psxButtonNames[PSX_BUTTONS_NO] PROGMEM = {  // PSX_BUTTONS_NO=16, as defined in PsxNewLib.h
-	buttonSelectName,
-	buttonL3Name,
-	buttonR3Name,
-	buttonStartName,
-	buttonUpName,
-	buttonRightName,
-	buttonDownName,
-	buttonLeftName,
-	buttonL2Name,
-	buttonR2Name,
-	buttonL1Name,
-	buttonR1Name,
-	buttonTriangleName,
-	buttonCircleName,
-	buttonCrossName,
-	buttonSquareName
+  buttonSelectName,
+  buttonL3Name,
+  buttonR3Name,
+  buttonStartName,
+  buttonUpName,
+  buttonRightName,
+  buttonDownName,
+  buttonLeftName,
+  buttonL2Name,
+  buttonR2Name,
+  buttonL1Name,
+  buttonR1Name,
+  buttonTriangleName,
+  buttonCircleName,
+  buttonCrossName,
+  buttonSquareName
 };
 
-byte psxButtonToIndex (PsxButtons psxButtons) {
-	byte i;
+byte psxButtonToIndex(PsxButtons psxButtons) {
+  byte i;
 
-	for (i = 0; i < PSX_BUTTONS_NO; ++i) {
-		if (psxButtons & 0x01) {
-			break;
-		}
+  for (i = 0; i < PSX_BUTTONS_NO; ++i) {
+    if (psxButtons & 0x01) {
+      break;
+    }
 
-		psxButtons >>= 1U;
-	}
+    psxButtons >>= 1U;
+  }
 
-	return i;
+  return i;
 }
 
-FlashStr getButtonName (PsxButtons psxButton) {
-	FlashStr ret = F("");
-	
-	byte b = psxButtonToIndex (psxButton);
-	if (b < PSX_BUTTONS_NO) {
-		PGM_BYTES_P bName = reinterpret_cast<PGM_BYTES_P> (pgm_read_ptr (&(psxButtonNames[b])));
-		ret = PSTR_TO_F (bName);
-	}
+FlashStr getButtonName(PsxButtons psxButton) {
+  FlashStr ret = F("");
 
-	return ret;
+  byte b = psxButtonToIndex(psxButton);
+  if (b < PSX_BUTTONS_NO) {
+    PGM_BYTES_P bName = reinterpret_cast<PGM_BYTES_P>(pgm_read_ptr(&(psxButtonNames[b])));
+    ret = PSTR_TO_F(bName);
+  }
+
+  return ret;
 }
 
-void dumpButtons (PsxButtons psxButtons) {
-	static PsxButtons lastB = 0;
+void dumpButtons(PsxButtons psxButtons) {
+  static PsxButtons lastB = 0;
 
-	if (psxButtons != lastB) {
-		lastB = psxButtons;     // Save it before we alter it
-		
-		Serial.print (F("Pressed: "));
+  if (psxButtons != lastB) {
+    lastB = psxButtons;  // Save it before we alter it
 
-		for (byte i = 0; i < PSX_BUTTONS_NO; ++i) {
-			byte b = psxButtonToIndex (psxButtons);
-			if (b < PSX_BUTTONS_NO) {
-				PGM_BYTES_P bName = reinterpret_cast<PGM_BYTES_P> (pgm_read_ptr (&(psxButtonNames[b])));
-				Serial.print (PSTR_TO_F (bName));
-			}
+    Serial.print(F("Pressed: "));
 
-			psxButtons &= ~(1 << b);
+    for (byte i = 0; i < PSX_BUTTONS_NO; ++i) {
+      byte b = psxButtonToIndex(psxButtons);
+      if (b < PSX_BUTTONS_NO) {
+        PGM_BYTES_P bName = reinterpret_cast<PGM_BYTES_P>(pgm_read_ptr(&(psxButtonNames[b])));
+        Serial.print(PSTR_TO_F(bName));
+      }
 
-			if (psxButtons != 0) {
-				Serial.print (F(", "));
-			}
-		}
+      psxButtons &= ~(1 << b);
 
-		Serial.println ();
-	}
+      if (psxButtons != 0) {
+        Serial.print(F(", "));
+      }
+    }
+
+    Serial.println();
+  }
 }
 
-void dumpAnalog (const char *str, const byte x, const byte y) {
-	Serial.print (str);
-	Serial.print (F(" analog: x = "));
-	Serial.print (x);
-	Serial.print (F(", y = "));
-	Serial.println (y);
+void dumpAnalog(const char* str, const byte x, const byte y) {
+  Serial.print(str);
+  Serial.print(F(" analog: x = "));
+  Serial.print(x);
+  Serial.print(F(", y = "));
+  Serial.println(y);
 }
 
 const char ctrlTypeUnknown[] PROGMEM = "Unknown";
@@ -111,151 +110,142 @@ const char ctrlTypeGuitHero[] PROGMEM = "Guitar Hero";
 const char ctrlTypeOutOfBounds[] PROGMEM = "(Out of bounds)";
 
 const char* const controllerTypeStrings[PSCTRL_MAX + 1] PROGMEM = {
-	ctrlTypeUnknown,
-	ctrlTypeDualShock,
-	ctrlTypeDsWireless,
-	ctrlTypeGuitHero,
-	ctrlTypeOutOfBounds
+  ctrlTypeUnknown,
+  ctrlTypeDualShock,
+  ctrlTypeDsWireless,
+  ctrlTypeGuitHero,
+  ctrlTypeOutOfBounds
 };
+
+const byte PIN_BUTTONPRESS = A0;
+const byte PIN_HAVECONTROLLER = A1;
 
 
 
 // PWM pins are 3, 5, 6, 9, 10, 11
-// need 4 PWM pins (2 motor, 1 pump, )
+// needed: 1 motor, 1 pump
 
-// define pins for PS2 controller
 const byte PIN_PS2_ATT = 4;
 const byte PIN_PS2_CMD = 3;
 const byte PIN_PS2_DAT = 2;
 const byte PIN_PS2_CLK = 5;
 
-const byte PIN_BUTTONPRESS = A0;
-const byte PIN_HAVECONTROLLER = A1;
-
-// define pins for motor driver
-const uint8_t PIN_L298_ENA;
-const uint8_t PIN_L298_IN1;
-const uint8_t PIN_L298_IN2;
-
-const uint8_t PIN_L298_ENB;
-const uint8_t PIN_L298_IN3;
-const uint8_t PIN_L298_IN4;
-
-const uint8_t M1 = 12;
-const uint8_t E1 = 10;
-
-// initialise PS2 controller
-PsxControllerBitBang<PIN_PS2_ATT, PIN_PS2_CMD, PIN_PS2_DAT, PIN_PS2_CLK> psx;
+PsxControllerBitBang<PIN_PS2_ATT, PIN_PS2_CMD, PIN_PS2_DAT, PIN_PS2_CLK>
+  psx;
 boolean haveController = false;
 
-// initialise motor driver
-L298NX2 motors(PIN_L298_ENA,
-               PIN_L298_IN1,
-               PIN_L298_IN2,
-               PIN_L298_ENB,
-               PIN_L298_IN3,
-               PIN_L298_IN4);
+const uint8_t MOTOR_DIRECTION = 12;
+const uint8_t MOTOR_SPEED = 10;
 
-void setup () {
-	fastPinMode (PIN_BUTTONPRESS, OUTPUT);
-	fastPinMode (PIN_HAVECONTROLLER, OUTPUT);
-	pinMode (M1, OUTPUT); // emulating L298N using UQ Innovate PCB (PWM effectively)
-	digitalWrite(M1, LOW);  //start with no movement
-	psx.begin();
+const uint8_t PUMP_ENABLE = 8;
+const uint8_t PUMP_SPEED = 9;
+static uint8_t PUMP_SPEED_INFO = 0;
 
-	delay (300);
+void setup() {
+  fastPinMode(PIN_BUTTONPRESS, OUTPUT);
+  fastPinMode(PIN_HAVECONTROLLER, OUTPUT);
 
-	Serial.begin (115200);
-	Serial.println (F("Ready!"));
+  pinMode(MOTOR_DIRECTION, OUTPUT);
+  digitalWrite(MOTOR_DIRECTION, LOW);
+  pinMode(MOTOR_SPEED, OUTPUT);
+  digitalWrite(MOTOR_SPEED, 0);
+
+  pinMode(PUMP_ENABLE, OUTPUT);
+  digitalWrite(PUMP_ENABLE, 0);
+  pinMode(PUMP_SPEED, OUTPUT);
+  analogWrite(PUMP_SPEED, 0);
+
+  psx.begin();
+
+  delay(300);
+
+  Serial.begin(115200);
+  Serial.println(F("Ready!"));
 }
- 
-void loop () {
-	
+
+void loop() {
   psx.read();
-
-  if (psx.buttonPressed(PSB_R2)) {
-    motors.forward();
-    digitalWrite(M1, LOW); // allow flow to motors
-    digitalWrite(E1, 255);
-    Serial.println(F("Moving FORWARDS"));
-  } else if (psx.buttonPressed(PSB_L2)) {
-    motors.backward();
-    digitalWrite(M1, HIGH);  // stop flow to motors
-    digitalWrite(E1, 255);
-    Serial.println(F("Moving BACKWARDS"));
-  } else {
-    motors.stop();
-    digitalWrite(E1, 0);
-    
-    Serial.println(F("Not moving"));
-  }
-
-  
-  // uncomment to debug controller
-  delay (1000 / 30); // 60Hz polling rate
-
+  MotorControl();
+  PumpControl();
+  delay(1000 / 60);  // 60Hz polling rate
 }
 
-void PSXInfoLoop () {
+void MotorControl() {
+  if (psx.buttonPressed(PSB_R2)) {
+    digitalWrite(MOTOR_DIRECTION, LOW);
+    digitalWrite(MOTOR_SPEED, 255);
+  } else if (psx.buttonPressed(PSB_L2)) {
+    digitalWrite(MOTOR_DIRECTION, HIGH);
+    digitalWrite(MOTOR_SPEED, 255);
+  } else {
+    digitalWrite(MOTOR_SPEED, 0);
+  }
+}
+
+void PumpControl() {
+  
+}
+
+void PSXInfoLoop() {
   static byte slx, sly, srx, sry;
-	
-	fastDigitalWrite (PIN_HAVECONTROLLER, haveController);
-	
-	if (!haveController) {  // if the controller isn't connected
-		if (psx.begin ()) {   // if the controller can be initialised
-			Serial.println (F("Controller found!"));
-			delay (300);
-			if (!psx.enterConfigMode ()) {
-				Serial.println (F("Cannot enter config mode"));
-			} else {
-				PsxControllerType ctype = psx.getControllerType ();
-				PGM_BYTES_P cname = reinterpret_cast<PGM_BYTES_P> (pgm_read_ptr (&(controllerTypeStrings[ctype < PSCTRL_MAX ? static_cast<byte> (ctype) : PSCTRL_MAX])));
-				Serial.print (F("Controller Type is: "));
-				Serial.println (PSTR_TO_F (cname));
 
-				if (!psx.enableAnalogSticks ()) {
-					Serial.println (F("Cannot enable analog sticks"));
-				}
-				
-				//~ if (!psx.setAnalogMode (false)) {
-				  //~ Serial.println (F("Cannot disable analog mode"));
-				//~ }
-				//~ delay (10);
-				
-				if (!psx.enableAnalogButtons ()) {
-					Serial.println (F("Cannot enable analog buttons"));
-				}
-				
-				if (!psx.exitConfigMode ()) {
-					Serial.println (F("Cannot exit config mode"));
-				}
-			}
-			
-			haveController = true;
-		}
-	} else {                // the controller is connected
-		if (!psx.read ()) {   // the controller can't be read
-			Serial.println (F("Controller lost :("));
-			haveController = false;
-		} else {              // the controller is responding
-			fastDigitalWrite (PIN_BUTTONPRESS, !!psx.getButtonWord ());
-			dumpButtons (psx.getButtonWord ());
+  fastDigitalWrite(PIN_HAVECONTROLLER, haveController);
 
-			byte lx, ly;
-			psx.getLeftAnalog (lx, ly);
-			if (lx != slx || ly != sly) {
-				dumpAnalog ("Left", lx, ly);
-				slx = lx;
-				sly = ly;
-			}
+  if (!haveController) {  // if the controller isn't connected
+    if (psx.begin()) {    // if the controller can be initialised
+      Serial.println(F("Controller found!"));
+      delay(300);
+      if (!psx.enterConfigMode()) {
+        Serial.println(F("Cannot enter config mode"));
+      } else {
+        PsxControllerType ctype = psx.getControllerType();
+        PGM_BYTES_P cname = reinterpret_cast<PGM_BYTES_P>(pgm_read_ptr(&(controllerTypeStrings[ctype < PSCTRL_MAX ? static_cast<byte>(ctype) : PSCTRL_MAX])));
+        Serial.print(F("Controller Type is: "));
+        Serial.println(PSTR_TO_F(cname));
 
-			byte rx, ry;
-			psx.getRightAnalog (rx, ry);
-			if (rx != srx || ry != sry) {
-				dumpAnalog ("Right", rx, ry);
-				srx = rx;
-				sry = ry;
-			}
-		}
-	}
+        if (!psx.enableAnalogSticks()) {
+          Serial.println(F("Cannot enable analog sticks"));
+        }
+
+        //~ if (!psx.setAnalogMode (false)) {
+        //~ Serial.println (F("Cannot disable analog mode"));
+        //~ }
+        //~ delay (10);
+
+        if (!psx.enableAnalogButtons()) {
+          Serial.println(F("Cannot enable analog buttons"));
+        }
+
+        if (!psx.exitConfigMode()) {
+          Serial.println(F("Cannot exit config mode"));
+        }
+      }
+
+      haveController = true;
+    }
+  } else {              // the controller is connected
+    if (!psx.read()) {  // the controller can't be read
+      Serial.println(F("Controller lost :("));
+      haveController = false;
+    } else {  // the controller is responding
+      fastDigitalWrite(PIN_BUTTONPRESS, !!psx.getButtonWord());
+      dumpButtons(psx.getButtonWord());
+
+      byte lx, ly;
+      psx.getLeftAnalog(lx, ly);
+      if (lx != slx || ly != sly) {
+        dumpAnalog("Left", lx, ly);
+        slx = lx;
+        sly = ly;
+      }
+
+      byte rx, ry;
+      psx.getRightAnalog(rx, ry);
+      if (rx != srx || ry != sry) {
+        dumpAnalog("Right", rx, ry);
+        srx = rx;
+        sry = ry;
+      }
+    }
+  }
 }
--- README.md
+++ README.md
@@ -1,19 +1,44 @@
 # ENGG1100 UFV Power & Control
 
-This repository contains the Arduino code which will be used to operate an unmanned firefighting vehicle ("UFV") prototype, based on an Arduino Uno, DualShock 2, and L298N Motor Driver, among other components.
+This repository contains the Arduino sketch which will operate the unmanned firefighting vehicle ("UFV") prototype for an ENGG1100 project.
 
-## Required functionality
-The following components must be manually controlled:
+An Arduino Uno (CH340), a DualShock 2 wireless gamepad, and both an L298N motor driver and a proprietary PCB courtesy of UQ Innovate, will facilitate the vehicle's functions.
+
+## What to implement
+Manual control of the following components:
+- ~~Drive~~
+	- ~~motor direction (L2/R2)~~
 - Fluid Delivery
-	- pump speed (D-Pad Up/Down)
-	- pump enable and disable (Square button)
-	- horizontal bearing (Left Analog stick)
-	- vertical tilt (Right Analog stick)
-- Drive
-	- motor speeds (fixed *or* L2/R2)
-	- motor direction (L2/R2)
+	- pump speed (L1/R1)
+	- pump enable and disable (Square/Cross)
+	- horizontal (D-Left/Right)
+	- vertical (D-Up/Down)
 
-## Utilised libraries
-PsxNewLib
-DigitalIO (dependency of PsxNewLib)
-L298N
+## Excerpt from [PsxNewLib][1] regarding how to connect controller
+[1]: https://github.com/SukkoPera/PsxNewLib/blob/master/README.md#tldr
+
+> ### TL;DR
+> - Power the controller at 3.3V.
+> - Don't bother with 'Motor Power' pin unless you want rumble.
+> - Connect all signals with proper level shifting and **1k pull-up resistors**.
+> 
+> ### Power
+> When plugged in a real PlayStation console, controllers are powered with 3.6V on pin 5. There are (too) many tutorials and videos out there that suggest that powering the controller at 5V is just fine, but the truth is that **it is not**: PlayStation controllers are not made to work at that voltage and **they WILL break** sooner or later. You have been warned.
+> 
+> Now, I know 3.6V regulators aren't exactly common. 3.3V seems close enough though, there are plenty of regulators for that voltage and my experience seems to suggest that all PSX controllers work just perfectly with this slight undervoltage, which is definitely safer than powering everything at 5V. Most conveniently, almost all classic Arduinos have an onboard 3.3V regulator. It is generally rated for only "little" current (say 50 mA?), but that will be enough.
+> 
+> ### Data
+> If you power the controller at 3.3V, all the I/O lines must also work at that voltage. Most Arduinos work at 5V, thus some level adjustment MUST be done. There are a few ways to achieve this, the easiest of which is probably using those bidirectional 4-channel level shifters you can get cheaply from China. You don't need bidirectionality, strictly speaking, still they are (almost) ideal for this application since they also provide pull-ups for the open-collector outputs and come in sets of 4.
+> 
+> The only issue is that these adapters generally come with 10k pull-up resistors, which are too weak and will create compatibility issues with some controllers. Therefore, **you MUST replace them with 1k ones** or put 1k in parallel to each.
+> 
+> Of course, if you use an Arduino board that works at 3.3V, you won't need any level shifting but **you will still need the pull-up resistors**, as the built-in ones are too weak.
+> 
+> A note on the *Acknowledge* pin: the original library did not use it and this is one of the reasons why it is not compatible with some controllers: it waits for a fixed interval between consecutive bytes instead of checking for the ACK pulse. Since some controllers are slower than others (typically older ones), they might not yet be ready for the next byte if the delay is not well calibrated (and it isn't).
+> 
+> The current version of PsxNewLib does the same, in that it does not use the ACK pin at all, but the delay was calibrated better. However this means that all controllers are polled much slower than they could be. Since this could be easily avoided by polling the ACK pin, **future versions of the library will require it** (the *devel* branch already does), so you are advised to wire it even if it is not necessary for the time being. If you don't want to waste a 4-channel module for a single signal, just connect it directly to an Arduino pin of choice and then connect a 1k resistor between it and 3.3V.
+
+## Some useful links
+
+[TinkerCAD simulation](https://www.tinkercad.com/things/dJLRxd4DNDG-mighty-fyyran-allis/editel?sharecode=YsRc_EckUVoIlAAD_wkochfdpelVsmDZIlHBKNxFIVg)<br>
+[Arduino Uno Pin-outs](https://www.circuito.io/blog/arduino-uno-pinout/)<br>
